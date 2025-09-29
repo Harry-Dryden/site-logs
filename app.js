e36c8b9bac723faf4bb7bed74eaf89a982b1bdc3
Harry-Dryden <harrydryden27@gmail.com>
2025-09-29 21:11:24 +0100

    creating js functions for saving

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-const locationAndTime = document.getElementById("locationAndTime");
-
 function getLocationAndTime() {
     navigator.geolocation.getCurrentPosition(success, error);
 }
 
 function success(position) {
-  locationAndTime.innerHTML = position.coords.latitude + "," + position.coords.longitude;
+  const entry = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+    accuracy: position.coords.accuracy,
+    timestamp: Date.now(),
+    timestampIso: new Date().toISOString(),
+    notes: ""
+  };
+
+  saveLog(entry);
+  displayLogs();
 }
 
 function error() {
   alert("Sorry, no position available.");
 }
+
+function saveLog(entry){
+  let logs = JSON.parse(localStorage.getItem("logs")) || []; //gets the logs from storage and JSON parses it OR creates an empty array
+  logs.push(entry);
+  const logsString = JSON.stringify(logs);
+  localStorage.setItem("logs", logsString);
+}
+
+function displayLogs(){
+  let logs = JSON.parse(localStorage.getItem("logs")) || []; //gets the logs from storage and JSON parses it OR creates an empty array
+  const logsContainer = document.getElementById("locationAndTime");
+  logsContainer.innerHTML = "";
+  logs.array.forEach(log => {
+    const log = document.create("p");
+    log.textContent = `${log.timestampIso} → ${log.latitude}, ${log.longitude} (±${log.accuracy}m)`;
+    container.appendChild(item);
+  });
+}
+
+window.onload = displayLogs;
